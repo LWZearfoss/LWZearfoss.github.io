ddad84ed6570b29c38789c60b706caec2789efa0
lwzearfoss@gmail.com <lwzearfoss@gmail.com>
2019-08-22 09:52:57 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2cfbe9d4cfe670f8c3bb.js"
+    "url": "webpack-runtime-720ce578c50c595d4750.js"
   },
   {
     "url": "app-2da29cce412461e59f12.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c8a4f3c3b937ddcaef74e53130cb13ec"
+    "revision": "7c0bfa11606bf55d642710c1003cc824"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "8a9bbb3be3f0b6549ba503c1f52b4122"
+    "revision": "7dc9e8ee6d95252f312ae72206b4846e"
   },
   {
     "url": "manifest.webmanifest",
